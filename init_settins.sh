743a7c4b7be3f6d91d48c1199d41c7a0f9d88cf7
silascript <silascript@outlook.com>
2023-08-27 03:00:15 +0800

    美化脚本显示

--- init_settins.sh
+++ init_settins.sh
@@ -1,51 +1,55 @@
 
-echo "开始初始化 Sublime..."
+echo -e "\e[36m 开始初始化 Sublime... \e[0m"
 
 #检测目录
 subldir=~/.config/sublime-text/'Installed Packages'
 
 if [ ! -d "$subldir" ];then
-  echo "目录不存在！"
-  echo "为了生成相关的目录,启动下SublimeText..."
+  echo -e "\e[33m \e[34m$subldir目录不存在！\e[0m"
+  echo -e "\e[36m 为了生成相关的目录,现在启动 SublimeText... \e[0m"
   subl
-else
-  echo "目录存在！"
 fi
 
-echo "开始设置..."
+echo -e "\e[36m 开始设置... \e[0m"
 
 #cd -
 
 # ------------------------------------------------------------------ #
 
-echo "复制settings..."
-cp -v ./base_settings.json ~/.config/sublime-text/Packages/User/Preferences.sublime-settings 
+echo -e "\e[36m 复制settings... \e[0m"
+echo -e "\e[36m 如果已存在的settings，将会被覆盖... \e[0m"
+cp -f -v ./base_settings.json ~/.config/sublime-text/Packages/User/Preferences.sublime-settings 
 
 if [ $? == 0 ];then
-	echo "settings复制成功！"
+	echo -e "\e[32m settings复制成功！\e[0m"
 else
-	echo "settings复制失败！"
+	echo -e "\e[31m settings复制失败！\e[0m"
 fi
 
 # ------------------------------------------------------------------ #
 
-# 安装 Package Control插件
-echo "安装 Package Control..."
+pcfile=$subldir/'Package Control.sublime-package'
+#echo $pcfile
 
-#curl https://packagecontrol.io/Package%20Control.sublime-package --output ~/.config/sublime-text/'Installed Packages'/'Package Control.sublime-package' --progress-bar
+if [ ! -f "$pcfile" ];then
+  # 安装 Package Control插件
+  echo -e "\e[36m 安装 Package Control... \e[0m"
 
-#curl https://packagecontrol.io/Package%20Control.sublime-package --output "$subldir/Package Control.sublime-package" --progress-bar
+  #curl https://packagecontrol.io/Package%20Control.sublime-package --output ~/.config/sublime-text/'Installed Packages'/'Package Control.sublime-package' --progress-bar
 
-#wget -P ~/.config/sublime-text/Installed\ Packages/ 'https://packagecontrol.io/Package Control.sublime-package'
+  #curl https://packagecontrol.io/Package%20Control.sublime-package --output "$subldir/Package Control.sublime-package" --progress-bar
 
-wget -P "$subldir/" 'https://packagecontrol.io/Package Control.sublime-package'
+  #wget -P ~/.config/sublime-text/Installed\ Packages/ 'https://packagecontrol.io/Package Control.sublime-package'
 
-
+  wget -P "$subldir/" 'https://packagecontrol.io/Package Control.sublime-package'
+else
+  echo -e "\e[32m Package Control 已经安装了！\e[0m"
+fi
 
 # 重启SublimeText
 #
 #echo "关闭 Sublime Text..."
 #pkill -f "sublime_text"
 
-echo "请自行重启 Sublime Text!"
+echo -e "\e[36m 请自行重启 Sublime Text! \e[0m"
 
