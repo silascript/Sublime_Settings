034607cb8f774cc9b886f0127b36b55f3cb6831f
silascript <silascript@outlook.com>
2023-08-27 22:53:17 +0800

    完善初始化脚本

--- subl_init.sh
+++ subl_init.sh
@@ -9,10 +9,25 @@ source ./subl_init_main.sh
 
 echo -e "\e[96m 初始化完成！\n \e[0m"
 
-# 重启 Sublime Text
-echo -e "\e[96m 重启 Sublime Text！\n \e[0m"
-# 关闭 Sublime Text
-pkill -f "sublime_text"
+# 如果已启动了 Sublime Text，就关闭，重启Sublime Text
+# 检测 sublime_text 进程数量
+# 大于1就意味着 Sublime Text 已经启动
+#subl_ps_count=`ps -ef|grep 'sublime_text'|grep -v grep|wc -l`
+subl_ps_count=`pgrep 'sublime_text' -l|wc -l`
+
+echo $subl_ps_count
+
+if [ $subl_ps_count -gt 0 ];then
+
+  # 关闭 Sublime Text
+  subl_pid=`pgrep 'sublime_text' -l|awk '{print $1}'`
+  #echo $subl_pid
+  kill $subl_pid
+
+  echo -e "\e[96m 重启 Sublime Text...\n \e[0m"
+  sleep 5
+fi
+
 # 启动 Sublime Text
 subl
 
--- subl_init_main.sh
+++ subl_init_main.sh
@@ -16,28 +16,46 @@ if [ ! -d "$subldir" ];then
   echo -e "\e[93m \e[94m$subldir \e[93m目录不存在！\e[0m"
   echo -e "\e[96m 为了生成相关的目录,现在启动 SublimeText...\n \e[0m"
   subl
+  # 休眠，等待Sublime Text 启动完成
+  sleep 5
+
 else
   echo -e "\e[92m 目录检测通过!\n \e[0m"
+fi
+
+# ------------------------------------------------------------------ #
+
+# 关闭 Sublime Text
+
+# 检测 Sublime Text 是否已经启动
+subl_pid_count=`pgrep 'sublime_text' -l|wc -l`
+
+if [ $subl_pid_count -gt 0 ];then
+  # 获取 sublime_text 的进程PID
+  subl_pid=`pgrep 'sublime_text' -l|awk '{print $1}'`
+  #echo $subl_pid
+
+  # 关闭 sublime_text 进程
+  echo -e "\e[96m Sublime Text已经启动，现先将其关闭...\n \e[0m"
+  kill $subl_pid 
+  sleep 5
+
 fi
 
 
 # ------------------------------------------------------------------ #
 
 # 复制 settings
-echo -e "\e[96m 复制 settings... \e[0m"
-echo -e "\e[96m 如果已存在的 settings，将会被覆盖... \e[0m"
+echo -e "\e[96m 复制 settings! \e[0m"
+echo -e "\e[96m 如果已存在的 settings，将会被覆盖...\n \e[0m"
 cp -f -v ./base_settings.json ~/.config/sublime-text/Packages/User/Preferences.sublime-settings 
 
 if [ $? == 0 ];then
-  echo -e "\e[92m \e[37mPreferences.sublime-settings\n \e[92m复制成功！\e[0m"
+  echo -e "\e[92m \e[37m \n Preferences.sublime-settings \e[92m复制成功！\n \e[0m"
   echo
 else
-  echo -e "\e[91m \e[37mPreferences.sublime-settings\n \e[91m复制失败！\e[0m"
+  echo -e "\e[91m \e[37m \n Preferences.sublime-settings \e[91m复制失败！\n \e[0m"
 fi
-
-# 关闭 Sublime Text
-pkill -f "sublime-text"
-
 
 # ------------------------------------------------------------------ #
 
@@ -59,6 +77,8 @@ if [ ! -f "$pcfile" ];then
 else
   echo -e "\e[92m Package Control 已经安装了！\n \e[0m"
 fi
+
+# ------------------------------------------------------------------ #
 
 # 重启SublimeText
 #
