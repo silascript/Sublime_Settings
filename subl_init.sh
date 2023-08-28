74ee157f6ac7ec2926e0e91a80226d096040aa7d
silascript <silascript@outlook.com>
2023-08-29 01:22:11 +0800

    重构初始化脚本

--- subl_init.sh
+++ subl_init.sh
@@ -3,34 +3,31 @@
 #		Sublime Text 初始化脚本			      #
 #-------------------------------------------------------------#
 
+#--------------------------引入脚本---------------------------#
+
 # 重置
-source ./subl_reset_main.sh
+source ./subl_reset.sh
 
 # 初始化
 source ./subl_init_main.sh
 
+#---------------------------主代码区--------------------------#
+
+# 复制 settings
+subl_cp_settings
+
+# 安装 Package Control
+install_packagecontrol
+
 echo -e "\e[96m 初始化完成！\n \e[0m"
 
-# 如果已启动了 Sublime Text，就关闭，重启Sublime Text
-# 检测 sublime_text 进程数量
-# 大于1就意味着 Sublime Text 已经启动
-#subl_ps_count=`ps -ef|grep 'sublime_text'|grep -v grep|wc -l`
-subl_ps_count=`pgrep 'sublime_text' -l|wc -l`
+# 重启Sublime Text
+subl_restart
 
-#echo $subl_ps_count
 
-if [ $subl_ps_count -gt 0 ];then
+#-------------------------------------------------------------#
 
-  # 关闭 Sublime Text
-  subl_pid=`pgrep 'sublime_text' -l|awk '{print $1}'`
-  #echo $subl_pid
-  kill $subl_pid
 
-  echo -e "\e[96m 重启 Sublime Text...\n \e[0m"
-  sleep 5
-fi
 
-# 启动 Sublime Text
-subl
 
 
