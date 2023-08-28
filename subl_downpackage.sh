10a1099729c58d96f34b0646bf3b6c39dd8695cb
silascript <silascript@outlook.com>
2023-08-28 21:05:24 +0800

    添加完善下载脚本主要功能

--- subl_downpackage.sh
+++ subl_downpackage.sh
@@ -3,85 +3,135 @@
 # 			下载包脚本
 # -------------------------------------------------- #
 
+
+
+# -------------------------------------------------- #
+# 		  全局变量区
+# -------------------------------------------------- #
+
 cachedir=".Cache"
 
+setting_sh_root=$PWD
+
+#----------------------------------------------------#
+
+
+#----------------------------------------------------#
+#		   函数定义区
+#----------------------------------------------------#
+
 # 生成缓存目录
-if [ ! -d $cachedir ];then
-  echo -e "\e[96m 新建\e[93m .Cache \e[96m目录,用于临时保存下载文件！ \e[0m"
-  mkdir .Cache
-fi
+function createCacheDir(){
+   
+  subl_cdir=$1
+  
+  # 检测缓存目录是否已经存在
+  if [ ! -d $subl_cdir ];then
+    echo -e "\e[96m 新建\e[93m .Cache \e[96m目录,用于临时保存下载文件！\n  \e[0m"
+    mkdir .Cache
+  else
+    echo -e "\e[93m $cachedir \e[96m已存在！\n \e[0m"
+  fi
 
-#----------------------------------------------------#
-
-
-setting_sh_root=$pwd
-
-echo $setting_sh_root
-
-#----------------------------------------------------#
-
-# 从github上clone
-function clone_repo_from_github(){
-cd $cachedir
-
-git clone $1
-
-#return 0
 }
+
 
 # 从仓库地址获取仓库名
 # 主要用于获取下载下来github项目的目录名
-function repoName(){
+function getRepoName(){
 
-#echo $1
+  # github 仓库地址
+  repo_addrs=$1
 
-# github 仓库地址
-repo_addrs=$1
+  # 判断仓库地址有没有.git
+  # 有就去除.git
+  if [ ${repo_addrs##*.}x = "git"x ];then
+    repo_addrs=${repo_addrs%.*}
+  fi
 
-# 使用空格替换斜杠作为分隔符，以此将字符串分割成数组
-arr_addrs=(${repo_addrs//\// })
+  # 使用空格替换斜杠作为分隔符，以此将字符串分割成数组
+  arr_addrs=(${repo_addrs//\// })
 
-# 取最后一个元素
-# 取最后一个元素的索引
-#repo_index=$((${#arr_addrs[@]}-1))
-repo_name=${arr_addrs[-1]}
+  # 取最后一个元素
+  # 取最后一个元素的索引
+  #repo_index=$((${#arr_addrs[@]}-1))
+  repo_name=${arr_addrs[-1]}
 
-echo $repo_name
+  echo $repo_name
 
 }
 
 
+# 通过github地址下载
+function download_by_github_address(){
+    # 下载
+    # 第一个参数是github地址
+    # 第二个参数是下载的目标目录路径
+    git clone $1 $2
+}
+
+
+# 构建 sublime-package 包 
+function buildPackage(){
+  
+  
+  # github 库的地址
+  github_addrs=$1
+
+  # 通过仓库地址获取仓库名称
+  # 这个名称亦为下载后的目录名
+  repoName=$(getRepoName $github_addrs)
+
+  # 下载后的项目路径
+  repo_path=$cachedir/$repoName
+
+  # 通过路径判断项目是否已经下载
+  if [ ! -d "$repo_path" ];then
+    echo -e "\e[96m 开始下载...\n \e[0m"
+    # 下载
+    download_by_github_address $github_addrs $repo_path
+
+    # 获取判断是否下载成功
+    if [ $? -eq 0 ];then
+      echo -e "\e[96m $repoName 下载成功！\n \e[0m"
+      echo -e "\e[96m $repoName 开始构建 sublime-package 包...\n \e[0m"
+
+      # 构建 sublime-package
+      
+      echo $PWD
+
+      # 进入到.Cache目录
+      # 为下载github库作准备
+      #cd $cachedir
+
+    else
+      echo -e "\e[92m 下载存在问题! \e[0m"
+    fi
+
+  else
+    echo -e "\e[96m \e[93m$repoName \e[96m项目已下载！\e[0m"
+  fi
+}
+
+#----------------------------------------------------#
+#			主代码区
 #----------------------------------------------------#
 
 # 下载包
 
-addrs="https://github.com/rexdf/ChineseLocalization"
+#addrs="https://github.com/rexdf/ChineseLocalization"
+addrs="https://github.com/rexdf/ChineseLocalization.git"
 
-# 通过仓库地址获取仓库名称
-# 这个名称亦为下载后的目录名
-repoName=$(repoName $addrs)
 
-repo_path=$cachedir/$repoName
+# 生成.Cache 目录
+createCacheDir $cachedir
 
-echo $repo_path
+# 下载及构建
+buildPackage $addrs
 
-if [ ! -d "$repo_path" ];then
-
-  echo "开始下载..."
-  #dl_result=$(clone_repo_from_github $addrs)
-  clone_repo_from_github $addrs
-
-  if [ $? -eq 0 ];then
-    echo getRepoName $addrs
-  else
-    echo -e "\e[92m 下载存在问题! \e[0m"
-  fi
-
-else
-  echo -e "\e[96m 项目已下载！\e[0m"
-fi
 
 #----------------------------------------------------#
+
 
 
 
--- test.sh
+++ test.sh
@@ -1,14 +1,42 @@
 
 
 
-s1="https://github.com/rexdf/ChineseLocalization"
+#s1="https://github.com/rexdf/ChineseLocalization"
 
 # 将/替换为空格，并按空格切割成数组
-arr1=(${s1//\// })
+#arr1=(${s1//\// })
 
 # 取最后一个个元素
 # 取数组长度
-arr_count=${#arr1[@]}
+#arr_count=${#arr1[@]}
 #echo $arr_count
-echo ${arr1[$arr_count-1]}
+#echo ${arr1[$arr_count-1]}
 
+# ---------------------------------------------------------------
+
+
+s1="https://github.com/rexdf/ChineseLocalization.git"
+s2="https://github.com/rexdf/ChineseLocalization"
+
+# 从左向右取第一个.后的字符串
+# 即取到的是 com/rexdf/ChineseLocalization.git
+echo "${s1#*.}"
+
+# 从左向右取最后一个.后的字符串
+# 即取到的是 git
+echo "${s1##*.}"
+
+# 从右向左取.后的第一个字符串
+# 即取到的是 https://github.com/rexdf/ChineseLocalization
+echo "${s1%.*}"
+
+# 从右向左取最后一个.后的字符串
+# 取到的是 http://github
+echo "${s1%%.*}"
+
+
+
+
+
+
+# ---------------------------------------------------------------
