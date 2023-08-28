f5d332f000550e7baf48b955774567a8dbb54856
silascript <silascript@outlook.com>
2023-08-29 00:52:01 +0800

    重构关闭重启代码

--- subl_reset_main.sh
+++ subl_reset_main.sh
@@ -1,24 +1,35 @@
 
 
-#检测目录
-subldir=~/.config/sublime-text
-sublcachedir=~/.cache/sublime-text
 
-if [ ! -d "$subldir" ];then
-  echo -e "\e[33m \e[93m \e[37m$subldir \e[93m目录不存在！\e[0m"
-else
-  # 删除 ~/.config/sublime-text
-  echo -e "\e[32m 将删除 $subldir 目录，并启动 Sublime Text，以便重新生成 $subldir \e[0m"
-  rm -rvf $subldir
-fi
+# 删除 Sublime Text 相关目录
+function delete_subl_dir(){
 
-if [ ! -d "$sublcachedir" ];then
-  echo -e "\e[33m \e[37m$sublcachedir \e[93m目录不存在！\e[0m"
-else
-  # 删除 ~/.cache/sublime-text
-  rm -rvf $sublcachedir
-fi
+  #检测目录
+  subldir=~/.config/sublime-text
+  sublcachedir=~/.cache/sublime-text
+
+  if [ ! -d "$subldir" ];then
+    echo -e "\e[33m \e[93m \e[37m$subldir \e[93m目录不存在！\e[0m"
+  else
+    # 删除 ~/.config/sublime-text
+    echo -e "\e[32m 将删除 $subldir 目录，并启动 Sublime Text，以便重新生成 $subldir \e[0m"
+    rm -rvf $subldir
+  fi
+
+  if [ ! -d "$sublcachedir" ];then
+    echo -e "\e[33m \e[37m$sublcachedir \e[93m目录不存在！\e[0m"
+  else
+    # 删除 ~/.cache/sublime-text
+    rm -rvf $sublcachedir
+  fi
+}
+
+
 
 
 # ------------------------------------------------------------------ #
+
+
+
+
 
